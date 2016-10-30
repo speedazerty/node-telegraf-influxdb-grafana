d7b10840bf08e4beddcce07b9495cc0930d42215
Yannick Cordinier <yannick.cordinier@ssense.com>
2016-10-30 18:16:41 -0400

    Add a file which sends a metric to statsD

---
+++ ContainerNotify.ts
@@ -0,0 +1,22 @@
+
+import * as StatsdClient from 'statsd-client';
+import * as process from 'process';
+import * as fs from 'fs';
+
+const clientHost: string = process.env.STATSD_HOST || "localhost";
+const clientPort: number = parseInt(process.env.STATSD_PORT) || 8125;
+
+const client = new StatsdClient({
+    host: clientHost,
+    port: clientPort,
+    debug: true
+});
+
+client.increment('up,host=localhost,path=/test/home');
+fs.appendFile('notified.event', `Event sent ${(new Date().toString())}`, (err: Error) => {
+    if (err) {
+        console.log(err);
+    }
+});
+
+process.exit(0);
